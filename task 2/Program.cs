2d14fc30a2ca57815e10fe21c982211d665b74da
NiKToc <niktoc20@gmail.com>
2023-05-25 18:43:45 +0500

    Сделал 2 задание

---
+++ task 2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear(); 
+Console.Write("Введите 1-ое число: "); 
+int a = int.Parse(Console.ReadLine()!); 
+Console.Write("Введите 2-ое число: "); 
+int b = int.Parse(Console.ReadLine()!); 
+if (a > b) 
+    Console.WriteLine($"{a} больше, чем {b}"); 
+else Console.WriteLine($"{b} больше, чем {a}");
