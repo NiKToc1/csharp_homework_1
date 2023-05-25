d89a93896ba84cf60c189adfb591c54f3f1adae7
NiKToc <niktoc20@gmail.com>
2023-05-25 19:32:01 +0500

    Сделал 4 задание

---
+++ task 4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear(); 
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-ое число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a > b && a > c) Console.WriteLine($"Максимальное число - {a}");
+else if (b > a && b > c) Console.WriteLine($"Максимальное число - {b}");
+else Console.WriteLine($"Максимальное число - {c}");
